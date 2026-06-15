6e45338861b09d53e49a55ada045b62f3466b422
DakyDevv <loriscoppola010@gmail.com>
2026-06-15 15:25:22 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "@daky.dev",
+        name: "Geometry Dash 2",
         // Activity type number (0 = Playing).
-        type: 1, 
+        type: 0, 
       },
     ],
   },
